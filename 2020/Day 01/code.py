f1394d00eddd99947c07e2dd9c0b98dc0990c70f
Extragornax <gaspardwi@yahoo.fr>
2020-12-21 16:57:30 +0100

    Day01 part two

--- 2020/Day 01/code.py
+++ 2020/Day 01/code.py
@@ -1,4 +1,19 @@
-def main():
+def part2():
+    file = open("input.txt")
+    data = []
+    for i in file:
+        data.append(i)
+    for l1 in data:
+        one = int(l1)
+        for l2 in data:
+            two = int(l2)
+            for l3 in data:
+                three = int(l3)
+                if one + two + three == 2020:
+                    print(one * two * three)
+                    return
+
+def part1():
     file = open("input.txt")
     data = []
     for i in file:
@@ -12,4 +27,7 @@ def main():
                 return
 
 
-main()
+print("One")
+part1()
+print("Two")
+part2()
