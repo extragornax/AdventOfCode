354032dbe8ef6443bfccf65a9aa25bbfee6cd983
Gaspard WITRAND <gaspard.witrand@davidson.fr>
2022-12-13 14:43:53 +0100

    day02 part2

--- 2022/Day02/src/main.rs
+++ 2022/Day02/src/main.rs
@@ -20,6 +20,10 @@ const REP_ROCK: &str = "X";
 const REP_PAPER: &str = "Y";
 const REP_SCISSORS: &str = "Z";
 
+const ASK_LOSE: &str = "X";
+const ASK_EQUAL: &str = "Y";
+const ASK_WIN: &str = "Z";
+
 fn get_points(line: Pair) -> i64 {
     let pts = match line.play.as_str() {
         ROCK => match line.reply.as_str() {
@@ -51,6 +55,57 @@ fn get_points(line: Pair) -> i64 {
     }
 }
 
+fn get_asked_reply(line: Pair) -> i64 {
+    match line.reply.as_str() {
+        ASK_WIN => match line.play.as_str() {
+            ROCK => get_points(Pair {
+                play: ROCK.to_string(),
+                reply: REP_PAPER.to_string(),
+            }),
+            PAPER => get_points(Pair {
+                play: PAPER.to_string(),
+                reply: REP_SCISSORS.to_string(),
+            }),
+            SCISSORS => get_points(Pair {
+                play: SCISSORS.to_string(),
+                reply: REP_ROCK.to_string(),
+            }),
+            _ => 0,
+        },
+        ASK_EQUAL => match line.play.as_str() {
+            ROCK => get_points(Pair {
+                play: ROCK.to_string(),
+                reply: REP_ROCK.to_string(),
+            }),
+            PAPER => get_points(Pair {
+                play: PAPER.to_string(),
+                reply: REP_PAPER.to_string(),
+            }),
+            SCISSORS => get_points(Pair {
+                play: SCISSORS.to_string(),
+                reply: REP_SCISSORS.to_string(),
+            }),
+            _ => 0,
+        },
+        ASK_LOSE => match line.play.as_str() {
+            ROCK => get_points(Pair {
+                play: ROCK.to_string(),
+                reply: REP_SCISSORS.to_string(),
+            }),
+            PAPER => get_points(Pair {
+                play: PAPER.to_string(),
+                reply: REP_ROCK.to_string(),
+            }),
+            SCISSORS => get_points(Pair {
+                play: SCISSORS.to_string(),
+                reply: REP_PAPER.to_string(),
+            }),
+            _ => 0,
+        },
+        _ => 0,
+    }
+}
+
 fn part_01() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let mut buf_reader = BufReader::new(file);
@@ -75,9 +130,7 @@ fn part_01() -> std::io::Result<()> {
 
     let pts = strat.into_iter().map(|x| get_points(x)).sum::<i64>();
 
-    println!("{:?}", pts);
-
-    // println!("PART 1: Answer {:?}", elves[0]);
+    println!("PART 1: Answer {:?}", pts);
     Ok(())
 }
 
@@ -87,15 +140,30 @@ fn part_02() -> std::io::Result<()> {
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
 
-    let mut strat: Vec<i64> = vec![];
+    let mut strat: Vec<Pair> = vec![];
     let split = contents.split("\n");
+    for line in split {
+        if line.len() == 0 {
+            continue;
+        } else {
+            let mut split = line.split(" ");
+            let play = split.next().unwrap();
+            let reply = split.next().unwrap();
+            strat.push(Pair {
+                play: play.to_string(),
+                reply: reply.to_string(),
+            });
+        }
+    }
 
-    // println!("PART 2: Answer {:?}", answer);
+    let pts = strat.into_iter().map(|x| get_asked_reply(x)).sum::<i64>();
+
+    println!("PART 2: Answer {:?}", pts);
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
     if let Ok(_) = part_01() {}
-    // if let Ok(_) = part_02() {}
+    if let Ok(_) = part_02() {}
     Ok(())
 }
