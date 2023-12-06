9eeffb872d04df0d0cf634eb9d3a469f8eb49bc5
Gaspard WITRAND <gaspard.witrand@davidson.fr>
2023-12-06 13:42:05 +0100

    Day03 part1 not correct answer

---
+++ 2023/Day03/src/main.rs
@@ -0,0 +1,143 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+fn is_number(c: char) -> bool {
+    match c {
+        '0'..='9' => true,
+        _ => false,
+    }
+}
+
+fn retrieve_number_from_pos(x: i64, y: i64, all: &Vec<Vec<char>>) -> i64 {
+    if x < 0 || y < 0 {
+        return 0;
+    }
+    let up_down = x;
+    let mut side_to_side = y;
+    loop {
+        if up_down < 0 || side_to_side < 0 {
+            break;
+        }
+        let c = all[up_down as usize][side_to_side as usize];
+        if is_number(c) {
+            side_to_side -= 1;
+            continue;
+        } else {
+            break;
+        }
+    }
+    side_to_side += 1;
+    let mut number_vec = vec![];
+    if side_to_side < 0 || side_to_side >= all[up_down as usize].len() as i64 {
+        return 0;
+    }
+    println!("Updown {:?} Sidetoside {:?} -> len {}", up_down, side_to_side, all[up_down as usize].len() as i64);
+    while (side_to_side < all[up_down as usize].len() as i64) && is_number(all[up_down as usize][side_to_side as usize]) {
+        number_vec.push(all[up_down as usize][side_to_side as usize]);
+        side_to_side += 1;
+    }
+    number_vec.clone().iter().collect::<String>().parse::<i64>().unwrap_or(0)
+}
+
+/*
+    539219 Too low
+ */
+fn part_01() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    let split = contents.split("\n");
+    let mut all: Vec<Vec<char>> = vec![];
+    for s in split {
+        let mut tmp: Vec<char> = vec![];
+        for c in s.chars() {
+            tmp.push(c);
+        }
+        all.push(tmp);
+    }
+
+    #[derive(Debug)]
+    struct SymbolsPos {
+        symbol: char,
+        x: i64,
+        y: i64,
+    }
+
+    let mut symbols: Vec<SymbolsPos> = vec![];
+    for (i, line) in all.iter().enumerate() {
+        for (j, c) in line.iter().enumerate() {
+            if *c != '.' && !is_number(*c) {
+                symbols.push(SymbolsPos {
+                    symbol: *c,
+                    x: i as i64,
+                    y: j as i64,
+                });
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    struct SymbolsNums {
+        symbol: char,
+        numbers: Vec<i64>,
+    }
+    let mut all_numbers_for_symbol: Vec<SymbolsNums> = vec![];
+
+    for s in &symbols {
+        let x = s.x;
+        let y = s.y;
+        let mut symbol_data = SymbolsNums {
+            symbol: s.symbol,
+            numbers: vec![],
+        };
+        if is_number(all[(x - 1) as usize][(y - 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x - 1, y - 1, &all));
+        }
+
+        if is_number(all[(x - 1) as usize][(y) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x - 1, y, &all));
+        }
+
+        if is_number(all[(x - 1) as usize][(y + 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x - 1, y + 1, &all));
+        }
+
+        if is_number(all[(x) as usize][(y - 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x, y - 1, &all));
+        }
+
+        if is_number(all[(x) as usize][(y + 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x, y + 1, &all));
+        }
+
+        if is_number(all[(x + 1) as usize][(y - 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x + 1, y - 1, &all));
+        }
+
+        if is_number(all[(x + 1) as usize][(y) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x + 1, y, &all));
+        }
+
+        if is_number(all[(x + 1) as usize][(y + 1) as usize]) {
+            symbol_data.numbers.push(retrieve_number_from_pos(x + 1, y + 1, &all));
+        }
+
+        symbol_data.numbers.dedup();
+
+        all_numbers_for_symbol.push(symbol_data);
+    }
+    // println!("All numbers {:?}", all_numbers_for_symbol);
+
+    let total = all_numbers_for_symbol.iter().map(|s|
+        s.numbers.iter().sum::<i64>()
+    ).sum::<i64>();
+
+    println!("Part 01: {:?}", total);
+    Ok(())
+}
+
+
+fn main() {
+    if let Ok(_) = part_01() {}
+}
