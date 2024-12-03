866f3a140f30d4bddde9ca8863c4c3ce8c11684d
Gaspard WITRAND <gaspard.witrand@ovhcloud.com>
2024-12-03 09:25:43 +0100

    Day03 part 2

--- 2024/Day03/src/main.rs
+++ 2024/Day03/src/main.rs
@@ -2,6 +2,55 @@ use regex::Regex;
 use std::fs::File;
 use std::io::{BufReader, Read};
 
+fn read_file_to_string() -> Result<String, std::io::Error> {
+    let file = File::open("input.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+fn step_02() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+    // Regex patterns for `mul`, `do`, and `don't` instructions
+    let re_instruction = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+
+    let mut mul_enabled = true;
+    let mut sum = 0;
+    let mut start_idx = 0;
+
+    while let Some(mat) = re_instruction.find(&contents[start_idx..]) {
+        let match_str = mat.as_str();
+
+        if match_str.starts_with("don't") {
+            mul_enabled = false;
+        } else if match_str.starts_with("do") {
+            mul_enabled = true;
+        } else {
+            if mul_enabled {
+                let extracted = &match_str[4..match_str.len() - 1];
+                // println!("extracted {extracted}");
+
+                let split = extracted.split(",").collect::<Vec<&str>>();
+
+                let x: i64 = split[0].parse().unwrap();
+                let y: i64 = split[1].parse().unwrap();
+                sum += x * y;
+            }
+        }
+        start_idx = start_idx + mat.start() + 1;
+    }
+
+    println!("Step 02: {sum}");
+
+    Ok(())
+}
+
 fn step_01() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let mut buf_reader = BufReader::new(file);
@@ -27,6 +76,10 @@ fn step_01() -> std::io::Result<()> {
 
 fn main() {
     if let Err(e) = step_01() {
+        println!("Error: {}", e);
+    }
+
+    if let Err(e) = step_02() {
         println!("Error: {}", e);
     }
 }
